11c5a9fbd46bbb69125ae2ae3232bc8ad50ae3e2
Brijesh Shah <brijeshror@gmail.com>
2017-04-14 18:02:57 +0530

    Updated merge_properties method if target have property like is_many

--- lib/jasonette/jason/body.rb
+++ lib/jasonette/jason/body.rb
@@ -2,17 +2,5 @@ module Jasonette
   class Jason::Body < Jasonette::Base
     property :header
     property :sections, true
-
-    def attributes!
-      attributes = super
-      attributes.delete "sections" if attributes.has_key?("sections")
-      if !@all_sections.nil?
-        attributes["sections"] = [] if attributes["sections"].nil? || attributes["sections"].empty?
-        @all_sections.each do |sections|
-          attributes["sections"] << sections.attributes!
-        end
-      end
-      attributes
-    end
   end
 end
